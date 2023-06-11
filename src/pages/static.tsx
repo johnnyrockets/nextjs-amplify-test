e333669cc506712e70be66b6133391376aac5ee0
John Bisignano <bisignano@hotmail.com>
2023-06-11 10:09:28 -0700

    feat: initial commit

---
+++ src/pages/static.tsx
@@ -0,0 +1,27 @@
+type FormattedDateType = {
+    formattedDate: string
+}
+
+export default function Home(
+    { formattedDate }: FormattedDateType
+) {
+    return (
+        <>
+            <h1>Static page</h1>
+            <p>This page is static. It was built on {formattedDate}.</p>
+            <p>
+                <a href="/ssr">View a server-side rendered page.</a>
+            </p>
+        </>
+    );
+}
+
+export async function getStaticProps() {
+    const buildDate = Date.now();
+    const formattedDate = new Intl.DateTimeFormat("en-US", {
+        dateStyle: "long",
+        timeStyle: "long",
+    }).format(buildDate);
+
+    return { props: { formattedDate } };
+}
